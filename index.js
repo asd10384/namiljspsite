e82215bc679ddaf5e3bc061a7f9468ee7cde3ac8
asd10384 <65000786+asd10384@users.noreply.github.com>
2021-03-30 16:46:57 +0900

    11

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 
+// https://miryang.dev/2019/04/25/nodejs-page-3/
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Session = require('express-session');
+const flash = require('connect-flash');
+var MongoDBStore = require('connect-mongodb-session')(Session);
 
 const app = express();
 const Route = require('./js/main');
+const { mongoose_url } = require('./config.json');
 
-app.use(bodyParser.urlencoded({extended : true}));
+const PORT = process.env.PORT || 80;
+const URL = process.env.MONGO_URL || mongoose_url;
 
+mongoose.connect(URL, {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+
+app.use(flash());
+
+// 세션
+var store = new MongoDBStore({ //세션을 저장할 공간
+     uri: mongoose_url, //db url
+     collection: 'sessions' //콜렉션 이름
+});
+
+store.on('error', function(error) {
+     console.log(error);
+});
+
+app.use(Session({
+     secret: 'namilsite', //세션 암호화 key
+     resave: false, //세션 재저장 여부
+     saveUninitialized: true,
+     rolling: true, //로그인 상태에서 페이지 이동 시마다 세션값 변경 여부
+     cookie: {maxAge: 1000 * 60 * 60}, //유효시간
+     store: store
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs'); //ejs 템플릿 엔진  연동 
+app.use(bodyParser.urlencoded({extended : true})); 
 
 app.use('/', Route);
 
-app.listen(80, function() {
+app.listen(PORT, function() {
      console.log('http://localhost');
 });
--- js/main.js
+++ js/main.js
@@ -1,8 +1,82 @@
-const express = require("express");
-const router = express.Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get("/login", (req, res) => res.render("login", {page: "login"}));
+const express = require("express");
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const router = express.Router();
+const User = require('../modules/user');
+
+router.get('/', (req, res) => res.render('index', {email: (req.user) ? req.user.name : ''}));
+router.get("/login", (req, res) => res.render("login", {message: req.flash('login_message')}));
 router.get("/signup", (req, res) => res.render("signup", {page: "signup"}));
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+})
+
+// 로그인 시작
+//로그인에 성공할 시 serializeUser 메서드를 통해서 사용자 정보를 세션에 저장
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+//사용자 인증 후 요청이 있을 때마다 호출
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField : 'password',
+    passReqToCallback : true //request callback 여부
+},
+function (req, email, password, done) {
+    User.findOne({email: email, password: crypto.createHash('sha512').update(password).digest('base64')}, function(err, user){
+        if (err) {
+            throw err;
+        } else if (!user) {
+            return done(null, false, req.flash('login_message','이메일 또는 비밀번호를 확인하세요.')); // 로그인 실패
+        } else {
+            return done(null, user); // 로그인 성공
+        }
+    });
+}
+));
+
+router.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: true}), // 인증 실패 시 '/login'으로 이동
+function (req, res) {
+    res.redirect('/');
+    //로그인 성공 시 '/'으로 이동
+});
+// 로그인 끝
+
+// 회원가입 시작
+router.post("/signup", (req, res, next) => {
+    console.log(req.body);
+    User.find({ email:req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                res.send('<script type="text/javascript">alert("이미 존재하는 이메일입니다."); window.location="/signup"; </script>');
+            } else {
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: crypto.createHash('sha512').update(req.body.password).digest('base64') 
+                });
+                user.save()
+                    .then(result => {
+                        console.log(result);
+                        res.redirect("/");
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
+        });
+});
+// 회원가입 끝
 
 module.exports = router;
